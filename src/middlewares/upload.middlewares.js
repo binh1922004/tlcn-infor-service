dc1f8ef884070aa72f8e5ac3a39023d0e57f7720
Binh Ho <147068448+binh1922004@users.noreply.github.com>
2026-05-31 00:01:35 +0700

    Increase file size limit for uploads to 100MB

--- src/middlewares/upload.middlewares.js
+++ src/middlewares/upload.middlewares.js
@@ -6,7 +6,7 @@ const storage = multer.memoryStorage();
 export const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
+    fileSize: 100 * 1024 * 1024, // 100MB
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
